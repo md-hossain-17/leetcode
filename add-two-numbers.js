3e2e902d14ebd5d165fcc0d9727f1fb1edd35c59
Md Hossain <mdahossain17@gmail.com>
2025-02-23 14:58:01 -0500

    2. Add Two Numbers

---
+++ add-two-numbers.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = (l1, l2) => {
+    const head = new ListNode(0);
+    let tail = head;
+    let carry = 0;
+    while (l1 || l2 || carry) {
+        let sum = carry;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        tail.next = new ListNode(sum % 10);
+        tail = tail.next;
+        carry = sum >= 10 ? 1 : 0;
+    }
+    return head.next;
+};
