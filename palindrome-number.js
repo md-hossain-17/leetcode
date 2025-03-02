fbe367c05dfa1876f382f6d9447478be4ee0b2ac
Md Hossain <mdahossain17@gmail.com>
2025-03-02 05:31:00 -0500

    9. Palindrome Number

---
+++ palindrome-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = (x) => {
+    if (x < 0) return false;
+    const original = x;
+    let reversed = 0;
+    while (x > 0) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return original === reversed;
+};
